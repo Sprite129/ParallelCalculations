713e3d99cc35ddb4a699661e20ecf573497cfa27
Sprite <patrondimon1290@gmail.com>
2025-02-27 21:15:16 +0200

    Made a menu

--- PAR_Lab1/main.cpp
+++ PAR_Lab1/main.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main() {
 
-	cout << "Creeping death" << endl;
+	int choice = 0;
+
+	cout << "Select matrix size" << endl;
+
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		cout << "Size 120" << endl;
+		break;
+
+	case 2:
+		cout << "Size 1200" << endl;
+		break;
+
+	case 3:
+		cout << "Size 12000" << endl;
+		break;
+
+	default:
+		cout << "invalid" << endl;
+		break;
+	}
 
 	return 0;
 }

a83f478dd738bd1a2d8374ef6c433fd4f2b3db53
Sprite <patrondimon1290@gmail.com>
2025-02-27 23:23:30 +0200

    threads logic

--- PAR_Lab1/main.cpp
+++ PAR_Lab1/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <chrono>
+#include <random>
+#include <thread>
+#include <vector>
+
 using namespace std;
+
+void threadsCalc(int size, int threadsNum)
+{
+	vector<vector<int>> matrix(size, vector<int>(size, 0));
+	vector<thread> threads;
+
+	int matrixElementsNum = size * size;
+	int chunkSize = (matrixElementsNum + threadsNum - 1) / threadsNum;
+
+	for (int i = 0; i < threadsNum; i++)
+	{
+		int begin = i * chunkSize;
+		int end = begin + chunkSize;
+		//threads.emplace_back();
+	}
+
+	for (auto& thread : threads)
+	{
+		thread.join();
+	}
+	threads.clear();
+
+	for (int i = 0; i < threadsNum; i++)
+	{
+		int begin = i * chunkSize;
+		int end = begin + chunkSize;
+		//threads.emplace_back();
+	}
+
+	for (auto& thread : threads)
+	{
+		thread.join();
+	}
+
+	if (size == 120) {
+		cout << "Matrix after processing:" << endl;
+		//printMatrix(matrix);
+	}
+}
 
 int main() {
 
 	int choice = 0;
+	int marixSize;
+	int threadsVariants[] = { 1, 3, 6, 12, 24, 48, 96, 192 };
 
 	cout << "Select matrix size" << endl;
 
@@ -30,3 +76,4 @@ int main() {
 
 	return 0;
 }
+
